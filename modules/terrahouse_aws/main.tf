3ccc842277da6557a1a5cb31ce54cd1f1d51d9c9
Emmanuel Adebola <emmanuelchiefsade@yahoo.com>
2023-10-30 15:13:08 +0000

    #25 refactor s3 bucket into the new terrahouse_aws_module

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
 }
-module "terrahouse_aws" {
-  source="./modules/terrahouse_aws"
-  user_uuid=var.user_uuid
-  bucket_name=var.bucket_name
-}
+#module "terrahouse_aws" {
+#source="./modules/terrahouse_aws"
+# user_uuid=var.user_uuid
+# bucket_name=var.bucket_name
+#}
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "website_bucket" {
